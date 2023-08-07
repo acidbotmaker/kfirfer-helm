b95b0169656e6ed527611481f0377171ae8bfac4
Ajay M <ajay@acid-tech.com>
2023-08-07 15:43:47 +0530

    Updated package urls

--- generate-packages.sh
+++ generate-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CHART_DIR="./charts"
 RELEASES_DIR="./releases"
-GITHUB_ROOT="https://github.com/acidbotmaker/kfirfer-helm.git"
+GITHUB_ROOT="https://github.com/acidbotmaker/kfirfer-helm/raw/elasticsearch-7.17.3/"
 
 echo "Clearing old builds"
 rm -rf $RELEASES_DIR/*.tgz
